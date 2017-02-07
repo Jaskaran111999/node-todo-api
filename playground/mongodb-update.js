76a82fb6141da73336478b4ab48823012b721f1d
Jaskaran111999 <jaskaran110398@gmail.com>
2017-02-07 17:37:04 +0530

    Update script added

---
+++ playground/mongodb-update.js
@@ -0,0 +1,35 @@
+const MongoClient = require('mongodb').MongoClient;
+//const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').findOneAndUpdate({
+  //   text: 'Walk the dog'
+  // }, {
+  //   completed: true
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndUpdate({
+      name: 'singh'
+    }, {
+      $set: {
+         name: 'Jaskaran Singh'
+      },
+      $inc: {age: 1}
+    }, {
+      returnOriginal: false
+    }).then((result) => {
+      console.log(result);
+    });
+
+
+  // db.close();
+});
