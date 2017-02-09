b5ddf1e15303589533c4ae182540added4f80174
Jaskaran111999 <jaskaran110398@gmail.com>
2017-02-09 20:32:22 +0530

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var mongoose = require('./db/mongoose.js');
 var Todo = require('./models/todo.js').Todo;
 var User = require('./models/user.js').User;
+var ObjectID = require('mongodb').ObjectID;
 
 var express = require('express');
 var bodyparser = require('body-parser');
@@ -20,6 +21,37 @@ app.post('/todos', (req, res) => {
 	}, (e) => {
 		res.status(400).send(e);
 	});
+});
+
+app.get('/todos', (req, res) => {
+	Todo.find().then((todos) => {
+		res.send(todos);
+	}, (e) => {
+		res.status(400).send(e);
+	});
+});
+
+// GET /todos/1231234
+
+app.get('/todos/:id', (req, res) => {
+	var id = req.params.id;
+	
+	if(!ObjectID.isValid(id)) {
+		console.log('Id not valid');
+		return res.status(404).send();
+	}
+	else {
+		Todo.findById(id).then((todos) => {
+			if(!todos) {
+				return res.status(404).send();
+			}
+			res.send(todos);
+		}, (e) => {
+			res.send(e);
+		}).catch((e) => {
+			res.status(400).send();
+		});
+	}
 });
 
 app.listen(3000, () => {
